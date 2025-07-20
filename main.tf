08d833faff2fdf139b8b755ee416ca1b4687966d
Lavendra Jeena (EXT-Nokia) <lavendra.jeena.ext@nokia.com>
2025-07-20 17:43:18 +0530

    terraform code

--- main.tf
+++ main.tf
@@ -10,9 +10,6 @@ terraform {
 provider "azurerm" {
   # Configuration options
   subscription_id = "4a8882d8-040a-43b9-81c8-844e324752c0"
-  client_id = "d5641da7-fee9-44ad-9edf-68804c3fc79b"
-  client_secret = "1ve8Q~dRqw1YTj4IKctXVJfjwQRV~Ped~6OFVaef"
-  tenant_id = "1fe21149-36d2-4c81-b9a8-d3314f4e9874"
   features {}
 }
 
